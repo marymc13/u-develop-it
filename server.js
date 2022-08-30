9bf73c0312031ec51e60dcdb74e992c28e89ab63
Mary Paul <marymc135@hotmail.com>
2022-08-29 21:44:05 -0400

    initial

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,19 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//COnnect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'username',
+        //your MySQL password
+        pasword: 'Paul@2023',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 //Default response for any other request(Not Found)
 app.use((req, res) => {
